3b85e0f1983fd88cfa827a9253319940114536c9
José Vieira Neto <josevieira.dev@gmail.com>
2017-01-31 13:26:59 -0200

    Update deploy-develop.sh

--- deploy-develop.sh
+++ deploy-develop.sh
@@ -12,11 +12,10 @@ else
    setup
 fi
 
-echo "update develop branch"
-cd /home/{YOUR_USER}/deploy/{YOUR_PROJECT}
+echo "update master branch"
+cd /home/{YOUR_USER}/deploy/develop
 git pull https://{YOUR_USER_GIT}:{YOUR_PASSWORD_GIT}@{URL_GIT}
 git checkout develop
-
 echo "build project"
 if mvn clean package; then
    echo build ok
@@ -26,12 +25,12 @@ else
 fi
 
 echo "moving file"
-mv -f  target/{my-project}.jar /home/{YOUR_USER}/deploy/develop
+mv -f  target/{JAR_NAME}.jar /home/{YOUR_USER}/deploy/develop
 
 echo "kill process"
-kill `ps -ef | grep "java -jar /home/{YOUR_USER}/deploy/develop/{YOUR_PROJECT}"| grep -v grep | awk '{ print $2 }'`
+kill `ps -ef | grep "java -jar /home/{YOUR_USER}/deploy/develop/{JAR_NAME}"| grep -v grep | awk '{ print $2 }'`
 
 echo "run application production"
-nohup java -jar /home/{YOUR_USER}/deploy/develop/{my-project}.jar >  /home/{YOUR_USER}/deploy/develop/server-dev.log 2>&1 &
+nohup java -jar /home/{YOUR_USER}/deploy/develop/{JAR_NAME}.jar >  /home/{YOUR_USER}/deploy/develop/server-dev.log 2>&1 &
 
 echo "deploy finish"
